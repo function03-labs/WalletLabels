21e62808596af85b7ff758fae67480e69ee45af1
Eelco Wiersma <contact@pagebakers.nl>
2022-02-20 16:19:33 +0000

    Use FormDialog

--- packages/app/src/modules/getting-started/pages/getting-started.tsx
+++ packages/app/src/modules/getting-started/pages/getting-started.tsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 import * as Yup from 'yup'
 import { useRouter } from 'next/router'
 
-import { Container, Text, useDisclosure } from '@chakra-ui/react'
+import { Container, Text, ModalFooter, useDisclosure } from '@chakra-ui/react'
 
-import { Form, Field, FormLayout, SubmitButton, Modal } from '@saas-ui/react'
+import { Field, FormLayout, SubmitButton, FormDialog } from '@saas-ui/react'
 import { Page } from '@saas-ui/pro'
 
 import { useCreateOrganizationMutation } from '@app/graphql'
@@ -22,12 +22,6 @@ export function GettingStartedPage(props: any) {
 
   const { mutateAsync: createOrganization } = useCreateOrganizationMutation()
 
-  const ref = React.useRef<HTMLFormElement>(null)
-
-  const handleSubmit = () => {
-    ref.current?.requestSubmit()
-  }
-
   const disclosure = useDisclosure()
 
   React.useLayoutEffect(() => {
@@ -37,51 +31,49 @@ export function GettingStartedPage(props: any) {
   return (
     <Page title="Getting started" fullWidth>
       <Container py="40">
-        <Modal
+        <FormDialog
           title="Let's get you set up"
           hideCloseButton
           hideOverlay
           isCentered
           motionPreset="slideInBottom"
-          {...disclosure}
-        >
-          <Form
-            ref={ref}
-            schema={schema}
-            defaultValues={{
-              name: '',
-            }}
-            onSubmit={(data) => {
-              return createOrganization({ name: data.name })
-                .then((result) => {
-                  const slug = result?.createOrganization?.slug
-                  if (slug) {
-                    disclosure.onClose()
-                    return slug
-                  } else {
-                    throw new Error('Could not create a new organization.')
-                  }
-                })
-                .then((slug) => router.push(`/app/${slug}`))
-            }}
-          >
-            <FormLayout mb="2">
-              <Text fontSize="lg">
-                We need a little more information before continuing.
-              </Text>
-
-              <Field name="name" label="Organization name" />
-
+          schema={schema}
+          defaultValues={{
+            name: '',
+          }}
+          footer={
+            <ModalFooter>
               <SubmitButton
                 label="Create organization"
                 isPrimary
-                onClick={handleSubmit}
                 size="md"
                 width="full"
               />
-            </FormLayout>
-          </Form>
-        </Modal>
+            </ModalFooter>
+          }
+          onSubmit={(data) => {
+            return createOrganization({ name: data.name })
+              .then((result) => {
+                const slug = result?.createOrganization?.slug
+                if (slug) {
+                  disclosure.onClose()
+                  return slug
+                } else {
+                  throw new Error('Could not create a new organization.')
+                }
+              })
+              .then((slug) => router.push(`/app/${slug}`))
+          }}
+          {...disclosure}
+        >
+          <FormLayout mb="2">
+            <Text fontSize="lg">
+              We need a little more information before continuing.
+            </Text>
+
+            <Field name="name" label="Organization name" />
+          </FormLayout>
+        </FormDialog>
       </Container>
     </Page>
   )
