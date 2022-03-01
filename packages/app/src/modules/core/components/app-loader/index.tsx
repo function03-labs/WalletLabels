af3b901651426d053a0f83367ab1367b358a556e
Eelco Wiersma <contact@pagebakers.nl>
2022-03-01 20:13:01 +0000

    Upgrade Saas UI, use updated Loader api

--- packages/app/src/modules/core/components/app-loader/index.tsx
+++ packages/app/src/modules/core/components/app-loader/index.tsx
@@ -1,47 +1,18 @@
 import * as React from 'react'
 
-import { chakra, ChakraProps } from '@chakra-ui/react'
-
 import { SaasUIGlyph } from '../logo/saas-ui-glyph'
 
-import { AnimatePresence, motion, MotionProps } from 'framer-motion'
-
-const Motion = chakra(motion.div)
-
-export interface AppLoaderProps
-  extends Omit<MotionProps, 'transition'>,
-    ChakraProps {
-  isLoading: boolean
-}
+import { Loader, LoaderProps } from '@saas-ui/react'
 
 /**
  * Show a fullscreen loading animation while the app is loading.
  */
-export const AppLoader: React.FC<AppLoaderProps> = (props) => {
-  const { isLoading, children, ...rest } = props
-
+export const AppLoader: React.FC<LoaderProps> = (props) => {
   return (
-    <AnimatePresence>
-      {isLoading && (
-        <Motion
-          position="fixed"
-          top="0"
-          right="0"
-          bottom="0"
-          left="0"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          bg="app.background"
-          zIndex="modal"
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          {...rest}
-        >
-          {children || <SaasUIGlyph width="48px" height="48px" isAnimating />}
-        </Motion>
-      )}
-    </AnimatePresence>
+    <Loader
+      {...props}
+      variant="fullscreen"
+      spinner={<SaasUIGlyph width="48px" height="48px" isAnimating />}
+    />
   )
 }
