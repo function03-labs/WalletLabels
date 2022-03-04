03d56875230be5f113a7984d927da6d64b6482ff
Eelco Wiersma <contact@pagebakers.nl>
2022-03-04 16:56:32 +0000

    Added UpdatePasswordDialog for security settings

--- packages/app/src/modules/settings/pages/account/security.tsx
+++ packages/app/src/modules/settings/pages/account/security.tsx
@@ -1,8 +1,38 @@
-import { Card, List, ListItem } from '@saas-ui/react'
+import { Card, List, ListItem, useModals } from '@saas-ui/react'
 import { Section } from '@saas-ui/pro'
 
-import { SettingsPage } from '@modules/core/components/settings-page'
+import { useDisclosure } from '@chakra-ui/hooks'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+
+import { SettingsPage } from '@modules/core/components/settings-page'
+
+import { UpdatePasswordDialog } from '../../components/update-password-dialog'
+
+function TwoFactorAuthItem() {
+  return (
+    <ListItem
+      onClick={() => null}
+      primary="Two-factor authentication"
+      tertiary="Not enabled"
+      action={<ChevronRightIcon />}
+    />
+  )
+}
+
+function PasswordListItem() {
+  const disclosure = useDisclosure()
+
+  const modals = useModals()
+
+  return (
+    <ListItem
+      onClick={() => modals.open?.(UpdatePasswordDialog)}
+      primary="Password"
+      tertiary="Last changed January 1st 2022"
+      action={<ChevronRightIcon />}
+    />
+  )
+}
 
 function AccountSignIn() {
   return (
@@ -13,18 +43,8 @@ function AccountSignIn() {
     >
       <Card>
         <List variant="settings">
-          <ListItem
-            onClick={() => null}
-            primary="Password"
-            tertiary="Last changed January 1st 2022"
-            action={<ChevronRightIcon />}
-          />
-          <ListItem
-            onClick={() => null}
-            primary="Two-factor authentication"
-            tertiary="Not enabled"
-            action={<ChevronRightIcon />}
-          />
+          <PasswordListItem />
+          <TwoFactorAuthItem />
         </List>
       </Card>
     </Section>
