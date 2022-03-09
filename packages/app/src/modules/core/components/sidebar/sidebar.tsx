c4f323058443474946bd14749ccea0adf154a52b
Eelco Wiersma <contact@pagebakers.nl>
2022-03-09 16:06:32 +0000

    Removed tooltip

--- packages/app/src/modules/core/components/sidebar/sidebar.tsx
+++ packages/app/src/modules/core/components/sidebar/sidebar.tsx
@@ -64,7 +64,6 @@ export const Sidebar = () => {
                 href={getPath()}
                 label="Dashboard"
                 icon={<FiHome />}
-                tooltip="Dashboard "
               />
               <SidebarLink
                 href={getPath('inbox')}
