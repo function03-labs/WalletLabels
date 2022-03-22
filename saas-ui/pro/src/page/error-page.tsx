3f415ccb4ed0673b44a720262e855f1e7d859d2a
Eelco Wiersma <contact@pagebakers.nl>
2022-03-22 13:41:02 +0000

    ErrorPage now has its own prop type

--- saas-ui/pro/src/page/error-page.tsx
+++ saas-ui/pro/src/page/error-page.tsx
@@ -2,7 +2,8 @@ import * as React from 'react'
 
 import { EmptyState, EmptyStateProps } from '@saas-ui/react'
 
-export const ErrorPage: React.FC<EmptyStateProps> = (props) => {
-  /* @ts-ignore: @todo this will get fixed in @saas-ui/layout */
+export type ErrorPageProps = EmptyStateProps
+
+export const ErrorPage: React.FC<ErrorPageProps> = (props) => {
   return <EmptyState flex="1" {...props} />
 }
