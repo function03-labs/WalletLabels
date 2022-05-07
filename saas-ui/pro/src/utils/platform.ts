f96b1e11c889a69c0912c7e0b5c79fc145e6ddd0
Eelco Wiersma <contact@pagebakers.nl>
2022-05-07 16:24:27 +0200

    Improved platformSelect types

--- saas-ui/pro/src/utils/platform.ts
+++ saas-ui/pro/src/utils/platform.ts
@@ -1,16 +1,10 @@
 type Platform = 'mac' | 'windows' | 'linux' | 'android' | 'ipad' | 'iphone'
-interface PlatformSelect {
-  mac?: any
-  windows?: any
-  linux?: any
-  android?: any
-  ipad?: any
-  iphone?: any
-}
 
-export const platformSelect = (
-  select: PlatformSelect,
-  fallback: any,
+type PlatformSelect<ReturnType> = Partial<Record<Platform, ReturnType>>
+
+export const platformSelect = <ReturnType>(
+  select: PlatformSelect<ReturnType>,
+  fallback: ReturnType,
   userAgent?: string,
 ) => {
   let ua = userAgent
@@ -20,7 +14,7 @@ export const platformSelect = (
 
   if (!ua) return fallback
 
-  const platforms: Record<string, RegExp> = {
+  const platforms: Record<Platform, RegExp> = {
     mac: /Mac/,
     windows: /Windows/,
     linux: /Linux|X11|OpenBSD/,
@@ -31,14 +25,15 @@ export const platformSelect = (
 
   let platform: Platform | undefined
   for (const id in platforms) {
-    if (platforms[id].test(ua)) {
+    if (platforms[id as Platform].test(ua)) {
       platform = id as Platform
       break
     }
   }
 
   if (platform && select[platform]) {
-    return select[platform]
+    return select[platform] || fallback
   }
+
   return fallback
 }
