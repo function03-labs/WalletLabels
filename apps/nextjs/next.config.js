1194b3fcd582d5c614930bec9414b2b11e9cffc2
Eelco Wiersma <contact@pagebakers.nl>
2022-05-23 17:02:30 +0200

    Output electron nextjs build in separate directory.

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -2,6 +2,8 @@ const withSvgr = require('next-svgr')
 
 const withWorkspaces = require('@saas-ui/next-workspaces')
 
+const isElectron = process.env.npm_package_name === 'electron-app'
+
 module.exports = withWorkspaces({
   workspaces: ['packages', 'saas-ui'],
   basePath: '../../',
@@ -11,9 +13,10 @@ module.exports = withWorkspaces({
       optimizeFonts: true,
       modern: true,
     },
+    distDir: isElectron ? '.nextron' : '.next',
     webpack: (config, options) => {
       const { isServer } = options
-      if (!isServer && process.env.npm_package_name === 'electron-app') {
+      if (!isServer && isElectron) {
         config.target = 'electron-renderer'
       }
 
