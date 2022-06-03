7c69564e829fca7ae5e6307f858ac04fada2679a
Eelco Wiersma <contact@pagebakers.nl>
2022-06-03 13:39:05 +0200

    Fixed type error

--- packages/app/src/modules/core/hooks/use-get-tenants.ts
+++ packages/app/src/modules/core/hooks/use-get-tenants.ts
@@ -8,7 +8,7 @@ export const useGetTenants = () => {
       id: organization.id,
       slug: organization.slug,
       label: organization.name || organization.id,
-      logo: organization.logo,
+      logo: organization.logo || undefined,
       href: `/app/${organization.slug}`,
     })) || []
   )
