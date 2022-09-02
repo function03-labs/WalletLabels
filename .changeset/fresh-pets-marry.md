b8a7a69e423d4cc0fbc41bd2c9dee6f9060d51da
Eelco Wiersma <contact@pagebakers.nl>
2022-09-02 08:52:47 +0200

    feat: renamed electron-app to desktop

---
+++ .changeset/fresh-pets-marry.md
@@ -0,0 +1,5 @@
+---
+'desktop': minor
+---
+
+Renamed electron to desktop
--- apps/desktop/nextron.config.js
+++ apps/desktop/nextron.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // specify an alternate main src directory, defaults to 'main'
   mainSrcDir: 'main',
   // specify an alternate renderer src directory, defaults to 'renderer'
-  rendererSrcDir: '../nextjs',
+  rendererSrcDir: '../web',
   // rendererSrcDir: './renderer',
 
   // main process' webpack config
