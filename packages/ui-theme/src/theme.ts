dbe61f35e11be27ffa51dbf3c693f4b87fc248e0
Eelco Wiersma <contact@pagebakers.nl>
2022-12-10 15:31:56 +0100

    feat: add pro glass theme

--- packages/ui-theme/src/theme.ts
+++ packages/ui-theme/src/theme.ts
@@ -1,22 +1,22 @@
 import { extendTheme } from '@chakra-ui/react'
 
-import { theme as baseTheme } from '@saas-ui/pro'
-import { withThemeColors } from '@saas-ui/pro'
+import { theme as baseTheme, withThemeColors } from '@saas-ui/pro'
+import { theme as glassTheme } from '@saas-ui/pro-theme-glass'
 
 import semanticTokens from './foundations/semantic-tokens'
 import components from './components'
 
 import colorScheme from './color-schemes/galaxy'
 // import colorScheme from './color-schemes/earth'
-
+console.log(glassTheme.semanticTokens)
 export const theme = extendTheme(
   {
-    semanticTokens,
+    semanticTokens: glassTheme.semanticTokens,
     components,
   },
   /**
    * Uncomment this to use any of the built-in color schemes.
    */
   // withThemeColors(colorScheme),
-  baseTheme,
+  glassTheme,
 )
