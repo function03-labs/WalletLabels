0b8bbdeb06eb621cf2cff1ce63b20c683a806feb
Pagebakers <eelco@appulse.nl>
2023-02-26 15:43:11 +0100

    chore: update docs

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -1,1 +1,7 @@
+/**
+ * The API client is a wrapper around the API implementation and allows us to easily switch between different API implementations.
+ *
+ * For example, the demo app uses @api/mocks by default.
+ * The export can be switched to @api/supabase or @api/graphql to use a different API implementation.
+ */
 export * from '@api/mocks'
