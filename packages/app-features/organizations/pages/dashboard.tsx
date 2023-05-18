c61b1274b4c4fd9f08dc32b4a695dbe3502883d5
Pagebakers <eelco@appulse.nl>
2023-05-18 10:33:17 +0200

    chore: fix checkout link

--- packages/app-features/organizations/pages/dashboard.tsx
+++ packages/app-features/organizations/pages/dashboard.tsx
@@ -67,7 +67,7 @@ export function DashboardPage() {
       />
       <ToolbarButton
         as="a"
-        href="https://saas-ui.lemonsqueezy.com/checkout?cart=f3e5aad8-3098-44b7-a45e-46b50d664087"
+        href="https://saas-ui.lemonsqueezy.com/checkout"
         label="Buy Pro"
         colorScheme="primary"
         variant="solid"
