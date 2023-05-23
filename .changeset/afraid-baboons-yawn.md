dfc288102c5d20198e7402ca0439ee54d0c6fb45
Pagebakers <eelco@appulse.nl>
2023-05-23 17:46:23 +0200

    fix: issue where useColumnVisiblity would not update

---
+++ .changeset/afraid-baboons-yawn.md
@@ -0,0 +1,5 @@
+---
+'@saas-ui-pro/react': patch
+---
+
+Fix issue where useColumnVisibility would not update
--- saas-ui/react/src/data-grid/data-grid.tsx
+++ saas-ui/react/src/data-grid/data-grid.tsx
@@ -529,40 +529,42 @@ export interface UseColumnVisibilityProps<Data, VisibleColumns = string[]> {
  */
 export const useColumnVisibility = <Data extends object>(
   props: UseColumnVisibilityProps<Data>,
+  deps?: React.DependencyList,
 ) => {
   const { columns, visibleColumns = [] } = props
-  const initRef = React.useRef(false)
 
-  const getVisibleColumns = React.useCallback(() => {
-    return (
-      columns.reduce<Record<string, boolean>>((memo, column) => {
-        let id = column.id
-        if (
-          !id &&
-          'accessorKey' in column &&
-          typeof column.accessorKey === 'string'
-        ) {
-          id = column.accessorKey
-        }
-        if (id) {
-          memo[id] =
-            column.enableHiding !== false ? visibleColumns?.includes(id) : true
-        }
-        return memo
-      }, {}) || {}
-    )
-  }, [columns, visibleColumns])
+  const getVisibleColumns = React.useCallback(
+    (visibleColumns: string[]) => {
+      return (
+        columns.reduce<Record<string, boolean>>((memo, column) => {
+          let id = column.id
+          if (
+            !id &&
+            'accessorKey' in column &&
+            typeof column.accessorKey === 'string'
+          ) {
+            id = column.accessorKey
+          }
+          if (id) {
+            memo[id] =
+              column.enableHiding !== false
+                ? visibleColumns?.includes(id)
+                : true
+          }
+          return memo
+        }, {}) || {}
+      )
+    },
+    [columns],
+  )
 
   const [columnVisibility, setColumnVisibility] = React.useState(
-    getVisibleColumns(),
+    getVisibleColumns(visibleColumns),
   )
 
   React.useEffect(() => {
-    if (!initRef.current) {
-      setColumnVisibility(getVisibleColumns)
-    }
-    initRef.current = true
-  }, [visibleColumns])
-
+    setColumnVisibility(getVisibleColumns(visibleColumns))
+  }, deps || [visibleColumns])
+  console.log('render')
   return columnVisibility
 }
