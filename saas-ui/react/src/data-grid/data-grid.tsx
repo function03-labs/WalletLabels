da28857c3c26957f229e01b1178698a48e52c74b
Pagebakers <eelco@appulse.nl>
2023-05-27 11:07:55 +0200

    chore: remove log

--- saas-ui/react/src/data-grid/data-grid.tsx
+++ saas-ui/react/src/data-grid/data-grid.tsx
@@ -106,6 +106,13 @@ export const useDataGridContext = <Data extends object>() => {
   return React.useContext(DataGridContext) as DataGridContextValue<Data>
 }
 
+/**
+ * Returns a memoized array of columns.
+ *
+ * @see https://tanstack.com/table/v8/docs/guide/column-defs#column-helpers
+ *
+ * @param columnHelper Tanstack table column helper
+ */
 export const useColumns = <Data extends RowData, Columns = unknown>(
   factory: <TData>(
     columnHelper: Pick<ColumnHelper<Data>, 'accessor' | 'display'>,
@@ -565,6 +572,6 @@ export const useColumnVisibility = <Data extends object>(
   React.useEffect(() => {
     setColumnVisibility(getVisibleColumns(visibleColumns))
   }, deps || [visibleColumns])
-  console.log('render')
+
   return columnVisibility
 }
