f83c915745393180ccbd96938f5d7a8908db53ed
Pagebakers <eelco@appulse.nl>
2023-06-10 11:58:59 +0200

    feat: add kanban example

---
+++ saas-ui/templates/kanban/components/Item/Item.tsx
@@ -0,0 +1,166 @@
+import React, { useEffect } from 'react'
+import classNames from 'classnames'
+import type { DraggableSyntheticListeners } from '@dnd-kit/core'
+import type { Transform } from '@dnd-kit/utilities'
+
+import { Handle, Remove } from './components'
+
+import styles from './Item.module.css'
+import { chakra, Card, CardBody } from '@chakra-ui/react'
+
+export interface Props {
+  dragOverlay?: boolean
+  color?: string
+  disabled?: boolean
+  dragging?: boolean
+  handle?: boolean
+  handleProps?: any
+  height?: number
+  index?: number
+  fadeIn?: boolean
+  transform?: Transform | null
+  listeners?: DraggableSyntheticListeners
+  sorting?: boolean
+  style?: React.CSSProperties
+  transition?: string | null
+  wrapperStyle?: React.CSSProperties
+  value: React.ReactNode
+  onRemove?(): void
+  renderItem?(args: {
+    dragOverlay: boolean
+    dragging: boolean
+    sorting: boolean
+    index: number | undefined
+    fadeIn: boolean
+    listeners: DraggableSyntheticListeners
+    ref: React.Ref<HTMLElement>
+    style: React.CSSProperties | undefined
+    transform: Props['transform']
+    transition: Props['transition']
+    value: Props['value']
+  }): React.ReactElement
+}
+
+export const Item = React.memo(
+  React.forwardRef<HTMLLIElement, Props>(
+    (
+      {
+        color,
+        dragOverlay,
+        dragging,
+        disabled,
+        fadeIn,
+        handle,
+        handleProps,
+        height,
+        index,
+        listeners,
+        onRemove,
+        renderItem,
+        sorting,
+        style,
+        transition,
+        transform,
+        value,
+        wrapperStyle,
+        ...props
+      },
+      ref,
+    ) => {
+      useEffect(() => {
+        if (!dragOverlay) {
+          return
+        }
+
+        document.body.style.cursor = 'grabbing'
+
+        return () => {
+          document.body.style.cursor = ''
+        }
+      }, [dragOverlay])
+
+      const styles2 = {
+        container: {},
+        item: {
+          w: 'full',
+          height: '100px',
+        },
+      }
+
+      return renderItem ? (
+        renderItem({
+          dragOverlay: Boolean(dragOverlay),
+          dragging: Boolean(dragging),
+          sorting: Boolean(sorting),
+          index,
+          fadeIn: Boolean(fadeIn),
+          listeners,
+          ref,
+          style,
+          transform,
+          transition,
+          value,
+        })
+      ) : (
+        <chakra.li
+          className={classNames(
+            styles.Wrapper,
+            fadeIn && styles.fadeIn,
+            sorting && styles.sorting,
+            dragOverlay && styles.dragOverlay,
+          )}
+          style={
+            {
+              ...wrapperStyle,
+              transition: [transition, wrapperStyle?.transition]
+                .filter(Boolean)
+                .join(', '),
+              '--translate-x': transform
+                ? `${Math.round(transform.x)}px`
+                : undefined,
+              '--translate-y': transform
+                ? `${Math.round(transform.y)}px`
+                : undefined,
+              '--scale-x': transform?.scaleX
+                ? `${transform.scaleX}`
+                : undefined,
+              '--scale-y': transform?.scaleY
+                ? `${transform.scaleY}`
+                : undefined,
+              '--index': index,
+              '--color': color,
+            } as React.CSSProperties
+          }
+          ref={ref}
+        >
+          <Card
+            sx={styles2.item}
+            className={classNames(
+              dragging && styles.dragging,
+              handle && styles.withHandle,
+              dragOverlay && styles.dragOverlay,
+              disabled && styles.disabled,
+              // color && styles.color,
+            )}
+            style={style}
+            data-cypress="draggable-item"
+            {...(!handle ? listeners : undefined)}
+            {...props}
+            tabIndex={!handle ? 0 : undefined}
+            mb="2"
+          >
+            <CardBody>
+              {value}
+              <span className={styles.Actions}>
+                {onRemove ? (
+                  <Remove className={styles.Remove} onClick={onRemove} />
+                ) : null}
+                {handle ? <Handle {...handleProps} {...listeners} /> : null}
+              </span>
+            </CardBody>
+          </Card>
+        </chakra.li>
+      )
+    },
+  ),
+)
