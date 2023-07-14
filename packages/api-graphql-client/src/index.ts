1e68981c3d0a1ad3b9833081cac6a05fa0978793
Pagebakers <eelco@appulse.nl>
2023-07-14 11:03:19 +0200

    feat: remove graphql implementation

--- packages/api-graphql-client/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './gql'
-export * from './gql/graphql'
-export * from './client'
-
-export * from './users'
-export * from './organizations'
-export * from './contacts'
-export * from './billing'
