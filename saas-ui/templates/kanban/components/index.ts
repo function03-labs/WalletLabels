5fae7dbbed29ae29c7454138dcc1789020184185
Pagebakers <eelco@appulse.nl>
2023-07-20 20:59:13 +0200

    chore: remove template

--- saas-ui/templates/kanban/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { KanbanColumn } from './kanban-column'
-export type { KanbanColumnProps } from './kanban-column'
-export { KanbanContainer } from './kanban-container'
-export { KanbanItem } from './kanban-item'
-export { Action, Handle, Remove } from './kanban-action'
-
-// export { List } from './List'
