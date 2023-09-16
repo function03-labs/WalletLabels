a600ca1de63285a27a01abbdfc8ec9e56414e18f
Pagebakers <eelco@appulse.nl>
2023-09-16 12:03:46 +0200

    fix: group by size

--- packages/app-features/contacts/pages/list.tsx
+++ packages/app-features/contacts/pages/list.tsx
@@ -258,9 +258,9 @@ export function ContactsListPage() {
       name="groupBy"
       value={groupBy}
       onChange={(value) => setGroupBy(value as string)}
-      size="sm"
+      size="xs"
     >
-      <SelectButton h="8">Status</SelectButton>
+      <SelectButton>Status</SelectButton>
       <SelectList>
         <SelectOption value="status">Status</SelectOption>
         <SelectOption value="type">Type</SelectOption>
