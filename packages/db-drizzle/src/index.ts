5766f85bb343c1f2d3d10f8607ffe08676e570e7
Pagebakers <eelco@appulse.nl>
2023-11-01 19:34:42 +0100

    chore: revert trpc and drizzle

--- packages/db-drizzle/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { neon, neonConfig } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
-
-import * as schema from './schema'
-
-neonConfig.fetchConnectionCache = true
-
-const sql = neon(process.env.DRIZZLE_DATABASE_URL!)
-export const db = drizzle(sql, { schema })
