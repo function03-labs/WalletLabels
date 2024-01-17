34a3371fbde4f545115059e9fac0f8a3e337c9e1
Pagebakers <eelco@appulse.nl>
2024-01-17 16:21:07 +0000

    chore: add feedback modal block

---
+++ saas-ui/blocks/src/modals/feedback-modal/feedback-modal.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import { Meta } from '@storybook/react'
+
+export default {
+  title: 'Templates/Modals/FeedbackModal',
+  decorators: [(Story) => <Story />],
+} as Meta
+
+export { default as FeedbackModal } from './feedback-modal'
