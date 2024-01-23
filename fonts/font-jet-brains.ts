4e99c8529851378c96d4d98cc8786f9ebd1903f9
FindMalek <malekgarahellalbus@gmail.com>
2024-01-23 18:49:16 +0100

    :construction:Building landing page | Fonts.

---
+++ fonts/font-jet-brains.ts
@@ -0,0 +1,10 @@
+import { JetBrains_Mono } from "next/font/google";
+
+const fontMonoJetBrains = JetBrains_Mono({
+  weight: "variable",
+  subsets: ["latin"],
+  variable: "--font-mono",
+  display: "swap",
+});
+
+export default fontMonoJetBrains;
---
+++ fonts/font-mono.ts
@@ -0,0 +1,10 @@
+import { DM_Mono } from "next/font/google";
+
+const fontMono = DM_Mono({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-mono",
+  display: "swap",
+});
+
+export default fontMono;
