e2c345c16cbfd217e5a6a9ffcd089fd9bf39e2f2
FindMalek <malekgarahellalbus@gmail.com>
2024-01-23 16:31:19 +0100

    :tada: Init Commit for Refactoring & Upgrading.

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://walletlabels.xyz',
-    generateRobotsTxt: true, // (optional)
-    sitemapSize: 7000,
-
-    // ...other options
-  }
