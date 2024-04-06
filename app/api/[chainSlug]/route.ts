987c712e0c0a40caf1172685c22e43cfd2be2f7b
FindMalek <malekgarahellalbus@gmail.com>
2024-04-06 21:11:32 +0100

    :zap: Increased serverless function timeout to 1 min.

--- app/api/[chainSlug]/route.ts
+++ app/api/[chainSlug]/route.ts
@@ -1,6 +1,5 @@
 import * as z from "zod";
 
-import getHistory from "@lib/get-history";
 import { connectToDatabase } from "@lib/mongodb";
 import { getChainEnv, normalizeLabels } from "@lib/utils";
 
@@ -13,6 +12,8 @@ const chainContextSchema = z.object({
     }),
   }),
 });
+
+export const maxDuration = 60;
 
 export async function GET(
   request: Request,
@@ -29,17 +30,8 @@ export async function GET(
     .limit(30)
     .toArray();
 
-  let response = normalizeLabels(labels);
-  const addresses = response.map((item) => item.address);
-
-  const history = await getHistory(addresses);
-  const data = response.map((item, index) => {
-    item.balanceHistory = JSON.stringify(history[item["address"]]);
-    return item;
-  });
-
   return new Response(
-    JSON.stringify({ data: data, revalidate: 60 * 60 * 24 }),
+    JSON.stringify({ data: normalizeLabels(labels), revalidate: 60 * 60 * 24 }),
     {
       status: 200,
       headers: {
