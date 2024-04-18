59dcb785726f0902bf35ba2d3f68eb19552c7dec
FindMalek <malekgarahellalbus@gmail.com>
2024-04-18 17:54:31 +0100

    :lipstick: Updated: Responsive Explore Card.

--- components/Bento.tsx
+++ components/Bento.tsx
@@ -55,20 +55,15 @@ const SkeletonOne = () => {
       </motion.div>
       <motion.div
         variants={variantsSecond}
-        className="relative ml-auto flex w-fit flex-row items-center space-x-2 rounded-full border border-neutral-100 bg-white p-2 pl-4 dark:border-white/[0.2] dark:bg-black"
+        className="relative ml-auto flex w-3/4 flex-row items-center space-x-2 rounded-full border border-neutral-100 bg-white p-2 dark:border-white/[0.2] dark:bg-black"
       >
         <div className="h-4 w-full rounded-full">
-          <div className="h-4 w-full rounded-full">
-            <p className="pr-8 text-xs text-gray-500 dark:text-gray-400 sm:hidden">
-              0x625e7708f30ca75bfd92586e17077590c60...
-            </p>
-            <p className="hidden pr-8 text-xs text-gray-500 dark:text-gray-400 sm:block md:text-right">
-              0x625e7708f30ca75bfd92586e17077590c60eb4cd
-            </p>
-          </div>
+          <p className="truncate pr-5 text-xs text-gray-500 dark:text-gray-400">
+            0x625e7708f30ca75bfd92586e17077590c60eb4cd
+          </p>
         </div>
 
-        <div className="absolute right-2 ml-4 size-6 shrink-0 rounded-full bg-gradient-to-l from-yellow-400 to-orange-600 " />
+        <div className="absolute right-2 size-6 shrink-0 rounded-full bg-gradient-to-l from-yellow-400 to-orange-600" />
       </motion.div>
 
       <motion.div
