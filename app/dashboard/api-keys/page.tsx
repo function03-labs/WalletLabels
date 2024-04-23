6b72383433904738de15e2ed8b7a73925b083713
Ahmed0-0Kallel <contactahmedkallel0@gmail.com>
2024-04-23 22:03:07 +0200

    ♻️ Refactored: Installed ShadcnUI

--- app/dashboard/api-keys/page.tsx
+++ app/dashboard/api-keys/page.tsx
@@ -1,12 +1,35 @@
 "use client"
 
+import { Box, Flex, IconButton, Table } from "@radix-ui/themes"
 import { motion } from "framer-motion"
+import { CopyIcon, EditIcon } from "lucide-react"
+import { LuLaptop } from "react-icons/lu"
 
 import { FADE_DOWN_ANIMATION_VARIANTS } from "@/config/design"
+import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
 import { ButtonSIWELogin } from "@/integrations/siwe/components/button-siwe-login"
 import { IsSignedIn } from "@/integrations/siwe/components/is-signed-in"
 import { IsSignedOut } from "@/integrations/siwe/components/is-signed-out"
+
+const apiKeys = [
+  {
+    id: 1,
+    name: "My API Key",
+    key: "657ccb71-b75496ea97-74581e2-c1d73469",
+  },
+  {
+    id: 2,
+    name: "My API Key",
+    key: "657ccb71-b75496ea97-74581e2-c1d73469",
+  },
+  {
+    id: 3,
+    name: "My API Key",
+    key: "657ccb71-b75496ea97-74581e2-c1d73469",
+  },
+]
 
 export default function PageDashboardApiKeys() {
   return (
@@ -24,7 +47,7 @@ export default function PageDashboardApiKeys() {
           <IsSignedOut>
             <div className="flex items-center gap-x-5 text-center">
               <span className="text-sm text-foreground">
-                Authenticate to access your api keys.
+                Authenticate to access your api keys. LuLaptop
               </span>
               <ButtonSIWELogin />
             </div>
